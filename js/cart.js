f0f075519d8edf9c9cbead167004ff0f4228a16c
Sibusiso <Sibusisodyan@gmail.com>
2024-07-22 20:50:27 +0200

    cart

--- js/cart.js
+++ js/cart.js
@@ -17,27 +17,41 @@ wish_length.innerHTML = wishlist.length
 // DISPLAY CART
 if(cart.length == 0){
     display.innerHTML=`
-    <p class="error">Cart is empty</p>
+    <center><p class="error">Cart is empty</p></center>
     `
 }else{
     cart.forEach((element,index) => {
         display.innerHTML += `
-            <div class="card_cart">
-            <div class="card-image">
-            <center>
-                 <img src="${element.image}">
-            </center>
-            </div>
-                <center><h4>${element.name}</h4><span class="price">R ${element.price}</span></center>
-                <center>
-                <button class="buttonRemove" onclick="Remove(${index})">Remove <i class="bi bi-trash"></i></button>
-                <button class="wishbtn" onclick="add_to_wishlist(${index})"><i class="bi bi-heart"></i></button>
-                </center>
-                <br>
-            </div>
+<div class="container text-center">
+  <div class="row align-items-start">
+    <div class="col">
+          <img src="${element.image}" alt="${element.name}+' image'">
+    </div>
+    <div class="col">
+      <p>${element.name}</p>
+      <p>${element.price}</p>
+    </div>
+    <div class="col">
+          <button class="btn-trash" onclick="Remove(${index})">
+        <i class="bi bi-trash3-fill"></i>
+        </button>
+    </div>
+  </div>
+</div>
         `
     });
 }
+function Remove(index) {
+    let position = cart[index].id
+    let target = cart.findIndex(obj => obj.id === position)
+     cart.splice(target,1)
+
+     localStorage.setItem("cart",JSON.stringify(cart))
+console.log('clicked')
+     cart = cart
+  
+      location.reload();
+ }
 // FUNCTIONS
 function add_to_wishlist(index){
 
@@ -58,7 +72,7 @@ function add_to_wishlist(index){
     wish_length.innerHTML = wishlist.length
 
 }
-
+document.querySelector('.items').innerHTML=`Items X ${cart.length}`
 function total(){
     let cartTotal = cart.reduce((accumulator, cart) => {
       let price = parseFloat(cart.price);
